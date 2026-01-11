e3574e8e5ca376817d1ad8a692fe5886c5d5ea6b
Dhanraj059 <sandadhanraj1998@gmail.com>
2026-01-11 11:40:22 -0600

    Create LoginTest.java

---
+++ selenium-java/sample-tests/LoginTest.java
@@ -0,0 +1,21 @@
+public class LoginTest {
+
+    public static void main(String[] args) {
+
+        // This is a sample Selenium automation structure
+        // Created for demonstration and interview purposes
+
+        System.out.println("Launching browser");
+        System.out.println("Navigating to login page");
+
+        // Example test steps
+        System.out.println("Entering username");
+        System.out.println("Entering password");
+        System.out.println("Clicking login button");
+
+        // Example validation
+        System.out.println("Verifying successful login");
+
+        System.out.println("Test completed successfully");
+    }
+}
